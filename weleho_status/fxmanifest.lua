21d030bf7c333c62e7b5d83687cb60e6f96c9225
weleho <57502552+weleho@users.noreply.github.com>
2021-06-04 12:45:57 +0300

    Added rdr3_warning

--- weleho_status/fxmanifest.lua
+++ weleho_status/fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Weleho'
 description 'Server Status'
